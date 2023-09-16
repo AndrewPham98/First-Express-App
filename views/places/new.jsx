4dfb7fe364a49b724acb8c08ce0c4e5514a74540
QanT Setsuna F. Seiei ✅ <AndrewPham1998@yahoo.com>
2023-09-15 18:23:41 -0700

    added jsx

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -46,5 +46,16 @@ function new_form (data) {
     )
 }
 
+let sumRatings = data.place.comments.reduce((tot, c) => {
+  return tot + c.stars
+}, 0)
+let averageRating = sumRatings / data.place.comments.length
+rating = (
+  <h3>
+  {Math.round(averageRating)} stars
+  </h3>
+)
+
+
 module.exports = new_form
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -13,65 +13,33 @@ function show (data) {
     </h3>
   )
   if (data.place.comments.length) {
-    let sumRatings = data.place.comments.reduce((tot, c) => {
-      return tot + c.stars
-    }, 0)
-    let averageRating = Math.round(sumRatings / data.place.comments.length)
-    let stars = ''
-    for (let i = 0; i < averageRating; i++) {
-      stars += '⭐️'
-    }
-    rating = (
-      <h3>
-        {stars} stars
-      </h3>
-    )
     comments = data.place.comments.map(c => {
       return (
         <div className="border col-sm-4">
-          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
-          <h4>{c.content}</h4>
-          <h3>
-            <stong>- {c.author}</stong>
-          </h3>
-          <h4>Rating: {c.stars}</h4>
-          <form method="POST" action={'/places/${data.place.id}/comment/${c.id}?_method=DELETE'}>
-            <input type="submit" className="btn btn-danger" value="Delete Comment" />
-          </form>
+          ...
         </div>
       )
-  
-    }
+    })
+  }
+  return (
+      <Def>
+        <main>
+          <div className="row">
+            ...
+            <div className="col-sm-6">
+              <h1>{ data.place.name }</h1>
+              <h2>
+                Rating
+              </h2>
+              {rating}
+              <br />
+              ...
+            </div>
+          </div>
+        </main>
+      </Def>
     )
   }
 
 
-  return (
-  <Def> 
-    <main>
-      <div className="row">
-        </div>
-        <div className="col-sm-6">
-          <img src={data.place.pic} alt={data.place.name} />
-          <h3>
-            Located in {data.place.city}, {data.place.state}
-          </h3>
-        </div>
-        <div className="col-sm-6">
-          ...
-          <h2>
-            Description
-          </h2>
-          <h3>
-            {data.place.showEstablished()}
-          </h3>
-          <h4>
-            Serving {data.place.cuisines}
-          </h4>
-          ...
-      </div>
-    </main>
-  </Def>
-  )
-  }
   module.exports = show
